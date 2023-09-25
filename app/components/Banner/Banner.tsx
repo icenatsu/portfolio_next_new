f303712b1a83c448e14120719a7d93acf5b98f51
ice <ice@xicex.fr>
2023-09-25 14:31:01 +0200

    Fix: Priorité image

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -10,7 +10,7 @@ const Banner = (): JSX.Element => {
         <div className={styles.banner}>
             <div className={styles.logo}><Image className={styles.logoImg} src={Logo} alt="Dessin d'une cruche inclinée" /></div>
             <div className={styles.rain} >
-                <Image className={styles.bannerbg} src={BannerBg} alt='Image animée de code' />
+                <Image className={styles.bannerbg} src={BannerBg} alt='Image animée de code' priority={true} />
             </div>
             <div className={styles.pot}>
                 <Image className={styles.PotImg} src={jarre} alt="Dessin d'une jarre" />
