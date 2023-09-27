879d3c52ca7d12f0a54050b1f05301d9f78531e4
ice <ice@xicex.fr>
2023-09-27 12:25:05 +0200

    Fix: animation vitesse

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -28,15 +28,15 @@ const About = (): JSX.Element => {
 
     // Animations gsap
     useEffect(() => {
-        animationSlideScrollToBottom("about", 0.1, 0.4, 50)
+        animationSlideScrollToBottom("about", 0, 0.2, 10)
     }, []);
 
     useEffect(() => {
-        animationSlideScrollToRight("aboutTitle", 0.1, 0.4, 0)
+        animationSlideScrollToRight("aboutTitle", 0.1, 0.3, 0)
     }, []);
 
     useEffect(() => {
-        animationSlideScrollToBottom("aboutDescription", 0.1, 0.4, 0)
+        animationSlideScrollToBottom("aboutDescription", 0.1, 0.3, 0)
     }, []);
 
 
