139a443a1252f7a1d606f166af532dd2f7a1d7e8
ice <ice@xicex.fr>
2023-09-28 12:41:02 +0200

    SEO: performances mobile

--- app/context/ThemeContext/ThemeContext.tsx
+++ app/context/ThemeContext/ThemeContext.tsx
@@ -18,18 +18,20 @@ export const ThemeContext = createContext<ThemeContext | null>(null);
 const ThemeContextProvider = ({ children }: PropsWithChildren): JSX.Element => {
     const [isDarkMode, setIsDarkMode] = useState<boolean>(window.matchMedia('(prefers-color-scheme: dark)').matches);
 
-    window
-        .matchMedia("(prefers-color-scheme: dark)")
-        .addEventListener('change', e => {
-            setIsDarkMode(e.matches)
-            if (e.matches) {
-                document.body.classList.add('dark');
-                document.body.classList.remove("light");
-            } else {
-                document.body.classList.add('light');
-                document.body.classList.remove("dark");
-            }
-        })
+    if (typeof window !== 'undefined') {
+        window
+            .matchMedia("(prefers-color-scheme: dark)")
+            .addEventListener('change', e => {
+                setIsDarkMode(e.matches)
+                if (e.matches) {
+                    document.body.classList.add('dark');
+                    document.body.classList.remove("light");
+                } else {
+                    document.body.classList.add('light');
+                    document.body.classList.remove("dark");
+                }
+            })
+    }
 
 
 
